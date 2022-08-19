e631a316971393eab5e3d9361d2a36e98af37ef9
Calvin <ncalvin67@gmail.com>
2022-08-19 11:17:33 +0300

    Completed I guess

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+// code your solution here 
+
+function superbowlWin(record) {
+    for (let W of record) {
+        while (W.result === "W") {
+            return W.year;
+        }
+    }
+}
+
+console.log(superbowlWin())
